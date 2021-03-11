b4895a7883e1130c51d78aa7e0f0842980d90eaa
Jon Ekse <jrekse@gmail.com>
2021-03-11 13:25:30 -0800

    made heroku server

--- Routes/apiroute.js
+++ Routes/apiroute.js
@@ -1,5 +1,38 @@
-const notes = require('../db/db.json');
+const savedNote = require('../db/db.json');
+const fs = require('fs');
 
 module.exports = (app) => {
-    
+
+    //gets the notes api and passes the response through the db.json
+    app.get('api/notes', (request, response) => response.json(savedNote));
+
+    //takes the note input and posts it to the server
+    app.post('api/notes', (request, response) => {
+        savedNote.push(request.body)
+        try {
+            fs.writeFileSync('./db/db.json', JSON.stringify(savedNote, null, 4))
+            response.json(true)
+        }
+        catch (error) {
+            response.json(error)
+        }
+    })
+
+    //
+    app.delete('/api/notes/:id', (request, response) => {
+        let deleteIndex = -1
+        savedNote.forEach((note, index) => {
+            if (note.id === request.params.id) {
+                deleteIndex = index
+            }
+        })
+        savedNote.splice(deleteIndex, 1)
+        try {
+            fs.writeFileSync('./db/db.json', JSON.stringify(savedNote, null, 4))
+            response.json(true)
+        }
+        catch (error) {
+            response.json(error)
+        }
+    })
 }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //defines express
 const express = require('express');
 const app = express();
-
+const apiroutes = require('./Routes/apiroute')
+const htmlroutes = require('./Routes/contentrout')
 //defines the port
 const PORT = process.env.PORT || 3000;
 
@@ -11,10 +12,8 @@ app.use(express.static('public'));
 //allows the app to use JSON parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//access to the Routes
-require('./Routes/apiroute');
-require('./Routes/contentrout');
+app.use('/', htmlroutes)
+app.use('/api', apiroutes)
 
 //listens for the port
 app.listen(PORT, () =>
